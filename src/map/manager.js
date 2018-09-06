9557acd6b5163fee85d951a332a051ff78641b9b
mbossX <mboss.mail@gmail.com>
2018-09-06 19:28:27 +0800

    add map and map d.ts

---
+++ src/map/manager.js
@@ -0,0 +1,16 @@
+
+import { Map } from '.';
+
+export default class MapManager {
+    load(path) {
+        let map = new Map();
+        return map;
+    }
+    create(seed) {
+        let map = new Map();
+        return map;
+    }
+    save(map) {
+
+    }
+}
